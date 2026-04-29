83d9b63662a57b663a782c2a6abb4e50752b9cc0
leejaebaek <leejaebaek5342@gmail.com>
2026-04-29 17:54:26 +0900

    Refactor fraction addition logic in solution function

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cpp
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cpp
@@ -5,29 +5,29 @@ using namespace std;
 
 vector<int> solution(int numer1, int denom1, int numer2, int denom2) {
     vector<int> answer;
-    int a = numer1 * denom2;
-    int b = numer2 * denom1;
-    int sum = a + b;
+    int top1 = numer1 * denom2;
+    int top2 = numer2 * denom1;
+    int top = a + b;
     
-    int c = denom1 * denom2;
+    int bottom = denom1 * denom2;
     for(int i=2;i<=1000;i++)
     {
-        if(sum<i || c<i) break;
-        if(sum%i==0 && c%i==0)
+        if(top<i || bottom<i) break;
+        if(top%i==0 && bottom%i==0)
         {
             while(1)
             {
-                if(sum%i!=0 || c%i!=0) break;
+                if(top%i!=0 || bottom%i!=0) break;
                 else
                 {
-                    sum/=i;
-                    c/=i;
+                    top/=i;
+                    bottom/=i;
                 }
                 
             }
         }
     }
-    answer.push_back(sum);
-    answer.push_back(c);
+    answer.push_back(top);
+    answer.push_back(bottom);
     return answer;
 }
