e9ce596739253d0d395df08b1255c0e263f1461c
leejaebaek <leejaebaek5342@gmail.com>
2026-05-04 23:59:47 +0900

    [level 0] Title: 모스부호 (1), Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+string solution(string letter) {
+    string answer = "";
+
+    vector<pair<string, char>> morse = {
+        {".-", 'a'}, {"-...", 'b'}, {"-.-.", 'c'}, {"-..", 'd'},
+        {".", 'e'}, {"..-.", 'f'}, {"--.", 'g'}, {"....", 'h'},
+        {"..", 'i'}, {".---", 'j'}, {"-.-", 'k'}, {".-..", 'l'},
+        {"--", 'm'}, {"-.", 'n'}, {"---", 'o'}, {".--.", 'p'},
+        {"--.-", 'q'}, {".-.", 'r'}, {"...", 's'}, {"-", 't'},
+        {"..-", 'u'}, {"...-", 'v'}, {".--", 'w'}, {"-..-", 'x'},
+        {"-.--", 'y'}, {"--..", 'z'}
+    };
+
+    stringstream ss(letter);
+    string code;
+
+    while (ss >> code) {
+        for (int i = 0; i < morse.size(); i++) {
+            if (code == morse[i].first) {
+                answer += morse[i].second;
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
