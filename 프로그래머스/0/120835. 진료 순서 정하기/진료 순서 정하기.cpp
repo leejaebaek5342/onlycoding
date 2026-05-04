255e93a8c3f3b66abc8390ced358e126a8344844
leejaebaek <leejaebaek5342@gmail.com>
2026-05-04 23:09:20 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
@@ -6,18 +6,12 @@ using namespace std;
 
 
 vector<int> solution(vector<int> emergency) {
-    vector<int> copy;
+    vector<int> copy = emergency;
     vector<int> answer;
     
     answer.resize(size(emergency),0);
     
-    for(int i=0;i<size(emergency);i++)
-    {
-        copy.push_back(emergency[i]);
-    }
-    
     sort(copy.rbegin(),copy.rend());
-    
     
     for(int i=0;i<size(emergency);i++)
     {
