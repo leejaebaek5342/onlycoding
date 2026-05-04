432c32001bedbed25329aaf43c2e46f43d1e42b4
leejaebaek <leejaebaek5342@gmail.com>
2026-05-05 00:28:22 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cpp
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -8,9 +8,18 @@ string solution(string rsp) {
    
     for(int i=0;i<size(rsp);i++)
     {
-        if(rsp[i] == '2') answer.push_back('0');
-        else if(rsp[i] == '0') answer.push_back('5');
-        else answer.push_back('2');
+        if(rsp[i] == '2')
+        {
+            answer.push_back('0');
+        }
+        else if(rsp[i] == '0') 
+        {
+            answer.push_back('5');
+        }
+        else 
+        {
+            answer.push_back('2');
+        }
     }
     return answer;
 }
