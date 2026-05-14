9b7725eb98e89fcb17862f2d492ea3bc37fa3a31
leejaebaek <leejaebaek5342@gmail.com>
2026-05-14 18:21:10 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.cpp
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.cpp
@@ -2,22 +2,13 @@
 #include <vector>
 
 using namespace std;
-int factorial(int num)
-{
-    if(num == 0 || num == 1) return 1;
-    return num * factorial(num-1);
-}
 
 int solution(int n) {
-    int cnt = 1;
-    int result = 0;
-    while(1)
+    int answer = 1;
+    int i;
+    for(i = 1; answer*i <= n; i++)
     {
-            result = factorial(cnt);
-            if(n >= result)
-            {
-                cnt++;
-            }
-            else return cnt - 1;
+        answer *= i;
     }
+    return i - 1;
 }
