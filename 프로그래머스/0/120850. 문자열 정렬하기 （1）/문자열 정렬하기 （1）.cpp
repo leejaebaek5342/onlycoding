49a0334c4dfa113233a189ba1935d9fc07efe867
leejaebaek <leejaebaek5342@gmail.com>
2026-05-14 13:22:09 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer;
+
+    for (const char &c : my_string) 
+    {
+        if (c >= '0' && c <= '9') 
+        {
+            answer.push_back(c - '0');
+        }
+    }
+
+    sort(answer.begin(), answer.end());
+
+    return answer;
+}
