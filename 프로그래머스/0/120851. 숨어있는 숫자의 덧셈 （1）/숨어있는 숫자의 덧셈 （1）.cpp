595ed885221191fef00191ef57bef2632ad19ace
leejaebaek <leejaebaek5342@gmail.com>
2026-05-14 13:23:10 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+
+    for (const char &c : my_string) 
+    {
+        if (c >= '1' && c <= '9') 
+        {
+            answer += c - '0';
+        }
+    }
+
+    return answer;
+}
