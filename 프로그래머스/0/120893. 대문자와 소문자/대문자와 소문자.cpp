7477c77a9af5e71574d5b754f89824bc42eb674b
leejaebaek <leejaebaek5342@gmail.com>
2026-05-15 11:14:59 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -7,7 +7,6 @@ string solution(string my_string) {
     string answer = "";
     for(int i = 0; i < size(my_string); i++)
     {
-        cout << (int) my_string[i];
         if(my_string[i] <= 90)
         {
             answer.push_back(my_string[i] + 32);
