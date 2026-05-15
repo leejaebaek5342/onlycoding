ba9ce369e7df32c38f0f79c6fa45c3dab27deef4
leejaebaek <leejaebaek5342@gmail.com>
2026-05-15 11:21:50 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.cpp
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.cpp
@@ -5,15 +5,15 @@
 using namespace std;
 
 int solution(vector<int> array, int n) {
-    int answer = 0;
     int mn = 101;
     
     sort(array.begin(),array.end());
+    
     for(int i = 0; i < size(array); i++)
     {
-        
         mn = min(mn, abs(n - array[i]));
     }
+    
     for(int i = 0; i < size(array); i++)
     {
         if(abs(n - array[i]) == mn)
