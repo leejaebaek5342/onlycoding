c3234197dd1b7ab85e7a084a1910b785b692f2be
leejaebaek <leejaebaek5342@gmail.com>
2026-05-18 15:03:13 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.04 ms, Memory: 4.32 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+int solution(string my_string) {
+    int sum = 0;
+    char save = '+';
+    string tmp = "";
+
+    stringstream ss(my_string);
+  
+    while(ss >> tmp)
+    {
+        if(tmp == "+" || tmp == "-")
+        {
+            save = tmp[0];
+        }
+        else
+        {
+            if(save == '+')
+            {
+                sum += stoi(tmp);
+            }
+            else if(save == '-')
+            {
+                sum -= stoi(tmp);
+            }
+        }
+    } 
+    return sum;
+}
