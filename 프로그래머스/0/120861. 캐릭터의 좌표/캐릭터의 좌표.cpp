daf907660186ab65599ba4bd64256de9d7b9731a
leejaebaek <leejaebaek5342@gmail.com>
2026-05-21 23:46:17 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> keyinput, vector<int> board) {
+    vector<int> answer(2,0);
+    int x = board[0] / 2, y = board[1] / 2;
+    
+    map<string,int> count= {
+        {"right", 1},
+        {"left", -1},
+        {"up", 1},
+        {"down",-1}
+    };
+    for(int i = 0; i < size(keyinput); i++)
+    {
+        if(answer[0] > x) continue;
+        if(keyinput[i] == "right" || keyinput[i] == "left")
+        {
+            if(abs(answer[0] + count[keyinput[i]]) <= x)
+            {
+                answer[0] += count[keyinput[i]];
+            }
+        }
+        else
+        {
+            if(abs(answer[1] + count[keyinput[i]]) <= y)
+            {
+                answer[1] += count[keyinput[i]];
+            }
+        }
+    }
+    
+    
+    
+    return answer;
+}
