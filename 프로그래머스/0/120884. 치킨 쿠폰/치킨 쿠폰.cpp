989e72d191ca875b8b7987d1ad77f07436db4f0a
leejaebaek <leejaebaek5342@gmail.com>
2026-06-01 12:10:42 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int chicken) {
+    int answer = 0;
+    int coupon = 0;
+    int service = 0;
+    while(coupon >= 10)
+    {
+        answer += chicken / 10;
+        coupon += chicken % 10;
+        chicken /= 10;
+    }
+    coupon += chicken;
+    
+    while(coupon >= 10)
+    {
+        answer += coupon / 10;
+        service = coupon / 10;
+        coupon %= 10;
+        coupon += service;
+    }
+    
+    return answer;
+}
