0d3785e3beadb09ae6709c976387fe46f44f8c3c
leejaebaek <leejaebaek5342@gmail.com>
2026-06-01 14:02:44 +0900

    [level 0] Title: k의 개수, Time: 3.93 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int i, int j, int k) {
+    int answer = 0;
+    for(int r = i; r <= j; r++)
+    {
+        string str = "";
+        str = to_string(r);
+        for(int z = 0; z < str.size(); z++)
+        {
+            if(str[z] == k + '0')
+            {
+                answer++;
+            }
+        }
+    }
+    return answer;
+}
